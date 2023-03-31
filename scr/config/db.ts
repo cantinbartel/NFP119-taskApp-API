a1f189b90f11fb6fd3a90435c237dad2c3a65a36
cantin bartel <cantinbartel.pro@gmail.com>
2023-03-31 11:02:31 +0200

    Non-null assertion operator fro procees.env vbariables and tsconfig outDir /dist

--- scr/config/db.ts
+++ scr/config/db.ts
@@ -1,23 +1,13 @@
 import { connect } from 'mongoose';
 
-
-declare var process : {
-    env: {
-      NODE_ENV: string
-      MONGO_URI: string
-    }
-}
-
 const dbConnect = async ():Promise<void> => {
-    const uri: string = process.env.MONGO_URI;
-
+    const uri: string = process.env.MONGO_URI!;
     try {
-        const cnx: any = await connect(uri);
+        await connect(uri);
         console.log('Mongo Connected');
     } catch (error){
         console.log(error);
-    }
-    
+    }   
 };
 
 export default dbConnect;
